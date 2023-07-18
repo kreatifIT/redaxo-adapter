f71f66190d2e9554943e0fbdf734069bf89a29ae
Daniel Planötscher <d.planoetscher@kreatif.it>
2023-07-18 12:57:22 +0200

    single wildcard query

--- src/wildcards.ts
+++ src/wildcards.ts
@@ -7,11 +7,31 @@ export async function getWildcards(clangId: string) {
     return data.wildCards as Wildcard[];
 }
 
+export async function getWildcard(
+    key: string,
+    clangId: string,
+): Promise<string> {
+    const { data } = await RedaxoAdapter.query(
+        REX_WILDCARD_QRY,
+        { key },
+        clangId,
+    );
+    return data.wildCard.replace;
+}
+
 export const REX_WILDCARDS_QRY = gql`
     query WildCards {
         wildCards {
             id
             wildcard
+            replace
+        }
+    }
+`;
+
+export const REX_WILDCARD_QRY = gql`
+    query WildCard($key: String!) {
+        wildCard(key: $key) {
             replace
         }
     }
